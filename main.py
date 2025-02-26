0571baa44dc2caabfc6b72eb36b58f5135d6aedd
Nhatdien <nhatkhonvl@gmail.com>
2025-02-26 15:23:41 +0700

    testing rabbitmq connectioni

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
+import pika, time, json, os, sys, dotenv
+from service.rabbitmq import RabbitMQ
 from service.ai_service_processor import AIProcessor
 from langchain_core.output_parsers import JsonOutputParser
-from fastapi import FastAPI
 from models.user import UserDataForGuidence, AIGuidanceResponse
 from utils.utils import init_mongo
 from config import settings as global_settings
-import uvicorn
 
-from contextlib import asynccontextmanager
+dotenv.load_dotenv()
+        
+def main():
+    print("testing")
+    rabbitmq = RabbitMQ()
+    def callback(ch, method, properties, body):
+       print(f"{ch}, {body}, {properties}")
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    app.state.mongo_client, app.state.mongo_db, app.state.mongo_collection = await init_mongo(
-    global_settings.MONGODB_DATABASE,
-    global_settings.mongodb_url.unicode_string(),
-    global_settings.MONGODB_COLLECTION,
-)
+    rabbitmq.channel.queue_declare("ai_tasks")
+    rabbitmq.channel.queue_declare("ai_response")
+
+    print(rabbitmq.__dict__)
+    
     try:
-        yield
+        print("Connection to RabbitMQ established successfully.")
+        rabbitmq.consume(queue_name='ai_tasks', callback=callback)
+        
+    except Exception as e:
+        print(f"Failed to establish connection to RabbitMQ: {e}")
+        sys.exit(1)
     finally:
-        pass
-
-app = FastAPI(lifespan=lifespan)
-@app.get("/")
-async def health_check():
-    print(app.state.mongo_db)
-    return "app is running"
-
-@app.post("/guidance")
-async def provide_guidence(user_data: UserDataForGuidence):
-    processor = AIProcessor()
-    res = processor.provide_guidence_process(user_data=user_data, parser=JsonOutputParser(pydantic_object=AIGuidanceResponse))
-
-    return res
+        rabbitmq.close()
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
