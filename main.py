e6a88e26b7c13edfb7323b83db0234d783d42045
Nhatdien <nhatkhonvl@gmail.com>
2025-05-06 15:49:07 +0700

    add vector store path

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pika, time, json, os, sys, dotenv
+import pika
+import time
+import json
+import os
+import sys
+import dotenv
 from service.rabbitmq import RabbitMQ
 from service.ai_service_processor import AIProcessor
 from langchain_core.output_parsers import JsonOutputParser
@@ -8,24 +13,27 @@ from config import settings as global_settings
 
 dotenv.load_dotenv()
 
+
 def callback(ch, method, properties, body):
-        ai_processor = AIProcessor()
-        parser = JsonOutputParser(pydantic_object=AIGuidanceResponse)
-        user_data = {
-            "current_week": 1,
-            "chatbot_interaction": "telling chatbot about his depression",
-            "emotion_tracking": "Feeling stressful lately"
-        }
-        user_pass_data = UserDataForGuidence(**user_data)
-        res = ai_processor.provide_guidence_process(user_data=user_pass_data, parser=parser)
-        print(res, type(res))
+    ai_processor = AIProcessor()
+    parser = JsonOutputParser(pydantic_object=AIGuidanceResponse)
+    user_data = {
+        "current_week": 1,
+        "chatbot_interaction": "telling chatbot about his depression",
+        "emotion_tracking": "Feeling stressful lately"
+    }
+    user_pass_data = UserDataForGuidence(**user_data)
+    res = ai_processor.provide_guidence_process(
+        user_data=user_pass_data, parser=parser)
+    for chunk in res:
         ch.basic_publish(exchange='',
-                        routing_key="ai_response",
-                        body=json.dumps(res),
-                        properties=pika.BasicProperties(
-                        delivery_mode=2,  # make message persistent
-                    ))
-        print(f"{ch}, {body}, {properties}")
+                         routing_key="ai_response",
+                         body=json.dumps(chunk),
+                         properties=pika.BasicProperties(
+                             delivery_mode=2,  # make message persistent
+                         ))
+        print(chunk)
+
 
 def main():
     print("testing")
@@ -35,17 +43,17 @@ def main():
     rabbitmq.channel.queue_declare("ai_response")
 
     print(rabbitmq.__dict__)
-    
+
     try:
         print("Connection to RabbitMQ established successfully.")
         rabbitmq.consume(queue_name='ai_tasks', callback=callback)
-        
+
     except Exception as e:
         print(f"Failed to establish connection to RabbitMQ: {e}")
         sys.exit(1)
     finally:
         rabbitmq.close()
-    
+
 
 if __name__ == "__main__":
     try:
--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel, Field
 
-#AI guidence request classes
+# AI guidence request classes
+
+
 class UserDataForGuidence(BaseModel):
-    current_week: int 
+    current_week: int
     chatbot_interaction: str
     emotion_tracking: str
 
+
 class AIGuidanceResponse(BaseModel):
     suggest_mindfulness_tip: str = Field(description="Tip for the user")
-    explanation: str = Field(description="Explanation for the tip provided")
+    explaination: str = Field(description="Explanation for the tip provided")
